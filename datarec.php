9e6b5bcadd224f5d24bfa9dbede181cfad14b29b
Rafael Karrer <10437610+rafaelkarrer@users.noreply.github.com>
2020-02-22 18:39:38 +0000

    Implement handling of array data in HTTP REQUEST and last time stamp

--- datarec.php
+++ datarec.php
@@ -2,15 +2,45 @@
 $nodeid = $_REQUEST["nodeid"];
 $temperature = $_REQUEST["temperature"];
 $humidity = $_REQUEST["humidity"];
+$dts = (int)$_REQUEST["dts"];
 $myfile = fopen($nodeid.".node", "a") or die("Unable to open file!");
+$tsfile = fopen($nodeid.".tim", "r+") or fopen($nodeid.".tim", "w+") or die("Unable to open file!");
+$dbgfile = fopen("debug.txt", "a") or die("Unable to open file!");
 
 $date = new DateTime();
 $timestamp = $date->getTimestamp();
-$datestr = date('Y-m-d\TH:i:s', $timestamp);
 
-$txt = $datestr.",".$temperature.",".$humidity."\n";
-fwrite($myfile, $txt);
+$npts = count($temperature);
+
+echo filesize($tsfile);
+
+//fseek($tsfile, 0, SEEK_SET);
+$ltimestamp = fread($tsfile, 10) or print_r("read failed");
+
+if ($ltimestamp != false) {
+    $dts = (int)(($timestamp - $ltimestamp) / $npts);
+}
+
+fseek($tsfile, 0, SEEK_SET);
+fwrite($tsfile, $timestamp);
+
+
+$dbgtxt = date('Y-m-d\TH:i:s', $timestamp).", ".$nodeid.", ".$ltimestamp.", ".$timestamp.", ".$npts.", ".$dts.", ".$_SERVER['REMOTE_ADDR']."\n";
+fwrite($dbgfile, $dbgtxt);
+
+print_r(date('Y-m-d\TH:i:s', $timestamp).", ".$nodeid.", ".$ltimestamp.", ".$timestamp);
+
+$timestamp = $timestamp - $dts*($npts-1);
+
+for ($i = 0; $i < $npts; $i++) {
+    $datestr = date('Y-m-d\TH:i:s', $timestamp + $dts*$i);
+    $txt = $datestr.",".$temperature[$i].",".$humidity[$i]."\n";
+    fwrite($myfile, $txt);
+}
+
+fclose($dbgfile);
 fclose($myfile);
+fclose($tsfile);
 
 # echo $_SERVER["REQUEST_METHOD"];
 # print_r( $_GET );
